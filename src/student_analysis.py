e74f025224ebe421be782aa19191850398206173
TheChanRProject <garpchoo@gmail.com>
2020-01-21 19:58:39 +0530

    Commiting my changes

--- src/student_analysis.py
+++ src/student_analysis.py
@@ -8,3 +8,10 @@ df_dict = {i[:-4]: pd.read_csv(f"{data_path}/{i}") for i in listdir(data_path)}
 union_df = df_dict['M191_student_data'][df_dict['M191_student_data']['Student Name'].isin(df_dict['M196_student_data']['Student Name']) == True]
 
 union_df['Student Name'].shape
+
+df_dict['M191_student_data']['School Name'].nunique()
+
+union_df['Student Name'].tolist()
+MyFinalList = union_df['Student Name'].tolist()
+new_students = [name for name in m196_students if name not in MyFinalList] 
+new_students
