4151bcb79e1e9f20557d646757c3e09759528ce1
TheChanRProject <garpchoo@gmail.com>
2020-01-23 13:09:03 +0530

    Commiting my changes

--- src/student_analysis.py
+++ src/student_analysis.py
@@ -1,14 +1,3 @@
-from os import getcwd, listdir
 import numpy as np
 import pandas as pd
-
-data_path = getcwd() + "/data"
-df_dict = {i[:-4]: pd.read_csv(f"{data_path}/{i}") for i in listdir(data_path)}
-
-union_df = df_dict['M191_student_data'][df_dict['M191_student_data']['Student Name'].isin(df_dict['M196_student_data']['Student Name']) == True]
-
-union_df['Student Name'].shape
-
-df_dict['M191_student_data']['School Name'].nunique()
-
-union_df['Student Name'].tolist()
+from os import getcwd, listdir
